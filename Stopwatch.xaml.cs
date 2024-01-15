af0d36e597039de863c568876ed760700c7e5f14
mishasamkov <150904235+mishasamkov@users.noreply.github.com>
2024-01-15 13:39:25 +0500

    Выполненная программа на оценку 4.САМКОВ

---
+++ Stopwatch.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace timelord_samkov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Stopwatch.xaml
+    /// </summary>
+    public partial class Stopwatch : UserControl
+    {
+        public DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        public float full_second = 0;
+        public bool start_stopwatch = false;
+        public Stopwatch()
+        {
+            InitializeComponent();
+            dispatcherTimer.Tick += TimerSecond;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+        }
+        private void TimerSecond(object sender, EventArgs e)
+        {
+            full_second++;
+
+            float hours = (int)(full_second / 60 / 60);
+            float minuts = (int)(full_second / 60) - (hours * 60);
+            float seconds = full_second - (hours * 60 * 60) - (minuts * 60);
+
+            string s_seconds = seconds.ToString();
+            if (seconds < 10) s_seconds = "0" + seconds;
+
+            string s_minuts = minuts.ToString();
+            if (minuts < 10) s_minuts = "0" + minuts;
+
+            string s_hours = hours.ToString();
+            if (hours < 10) s_hours = "0" + hours;
+
+            time.Content = s_hours + ":" + s_minuts + ":" + s_seconds;
+        }
+        private void StopWatchClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void StartStopwatch(object sender, RoutedEventArgs e)
+        {
+            if (start_stopwatch == false)
+            {
+                full_second = 0;
+                dispatcherTimer.Start();
+                start_stopwatch = true;
+                start.Content = "Стоп";
+                time.Content = "00:00:00";
+                interval.Items.Clear();
+            }
+            else
+            {
+                dispatcherTimer.Stop();
+                start_stopwatch = false;
+                start.Content = "Начать";
+            }
+        }
+        private void IntervalClick(object sender, RoutedEventArgs e)
+        {
+            if (start_stopwatch == true)
+            {
+                interval.Items.Add($"Инт. №{interval.Items.Count + 1} {time.Content}");
+            }
+        }
+        private void TimerClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow.mainWindow.OpenPages(MainWindow.pages.timer);
+        }
+    }
+}
